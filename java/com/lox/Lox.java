a1cf250326309b1fb2e32c67fce9aaadb19a7c2d
JasonJ2021 <3200104166@zju.edu.cn>
2022-11-26 21:36:25 +0800

    scanner finished

--- java/com/lox/Lox.java
+++ java/com/lox/Lox.java
@@ -1,7 +1,61 @@
 package com.lox;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Lox {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1){
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        }else if(args.length == 1){
+            runFile(args[0]);
+        }else {
+            runPrompt();
+        }
+    }
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path)); // get all bytes of source file
+        run(new String(bytes , Charset.defaultCharset()));
+        if(hadError){
+            // If we have a error , we should exit with an exit code
+            System.exit(65);
+        }
+    }
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        while(true){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null)break;
+            run(line);
+            // A line error shouldn't terminate the interactive environment
+            hadError = false;
+        }
+    }
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+    static void error(int line , String message){
+        report(line,"" , message);
+    }
+    private static void report(int line , String where , String message){
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
 }
---
+++ java/com/lox/Scanner.java
@@ -0,0 +1,205 @@
+package com.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.lox.TokenType.*;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<Token>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    // 比较简陋，1z?
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+    private void identifier(){
+        while(isAlphaNumeric(peek()))advance();
+        String lexeme = source.substring(start , current);
+        TokenType type = keywords.get(lexeme);
+        if(type == null){
+            type = IDENTIFIER;
+        }
+        addToken(type);
+    }
+    private boolean isAlpha(char c){
+        return (c <= 'Z' && c >= 'A') || (c <= 'z' && c >= 'a') || c =='_';
+    }
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+    private boolean isDigit(char c) {
+        return c <= '9' && c >= '0';
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while (!isAtEnd() && peek() != '"') {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated stirng.");
+            return;
+        }
+        advance(); // comsume the end "
+        String literal = source.substring(start + 1, current - 1);
+        addToken(STRING, literal);
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String lexema = source.substring(start, current);
+        tokens.add(new Token(type, lexema, literal, line));
+    }
+
+    private char advance() {
+        return this.source.charAt(current++);
+    }
+}
