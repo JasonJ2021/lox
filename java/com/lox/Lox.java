8bde1be7e74effd62d514221ff60834dc91ffdae
JasonJ2021 <3200104166@zju.edu.cn>
2022-12-01 20:38:21 +0800

    Expression Evaluate finished

--- java/com/lox/Lox.java
+++ java/com/lox/Lox.java
@@ -11,7 +11,8 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
-
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
     public static void main(String[] args) throws IOException {
         if(args.length > 1){
             System.out.println("Usage: jlox [script]");
@@ -29,6 +30,9 @@ public class Lox {
             // If we have a error , we should exit with an exit code
             System.exit(65);
         }
+        if(hadRuntimeError){
+            System.exit(70);
+        }
     }
     private static void runPrompt() throws IOException{
         InputStreamReader input = new InputStreamReader(System.in);
@@ -45,17 +49,34 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        for(Token token : tokens){
-            System.out.println(token);
-        }
+//        for(Token token : tokens){
+//            System.out.println(token);
+//        }
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parser();
+        if(hadError)return;
+//        System.out.println(new AstPrinter().print(expr));
+        interpreter.interpret(expr);
     }
     static void error(int line , String message){
         report(line,"" , message);
+    }
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
     private static void report(int line , String where , String message){
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message
         );
         hadError = true;
+    }
+    static void error(Token token ,String message){
+        if(token.type == TokenType.EOF){
+            report(token.line,  " at end" , message);
+        }else{
+            report(token.line, "at '"+token.lexeme+"'",message);
+        }
     }
 }
